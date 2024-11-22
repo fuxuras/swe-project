ef84b4cf476c2b6c26686a30274b095003629d55
fuxuras <furkancanokal1@gmail.com>
2024-11-23 00:03:56 +0300

    feat: initial commit

---
+++ src/test/java/com/swe/libary_project/LibaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe.libary_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
