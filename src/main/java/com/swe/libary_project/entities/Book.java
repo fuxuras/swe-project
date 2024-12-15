cb44491a7a263ad6d03628cf50296b595f1e603b
bircerenimo <fatmaceren2002@gmail.com>
2024-12-15 04:12:30 +0300

    feat: Book entitiy is added.

---
+++ src/main/java/com/swe/libary_project/entities/Book.java
@@ -0,0 +1,34 @@
+package com.swe.libary_project.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    private String genre;
+    private String status; // (Available, Borrowed, Reserved)
+
+    @Column(length = 500)
+    private String synopsis; // Özet
+
+    private LocalDate publicationDate;
+
+}
