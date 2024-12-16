28442691b2ed6734d179eaa8a00af92eace65e71
BARKAN ANIL TEKE <200315078@ogr.cbu.edu.tr>
2024-12-17 01:07:27 +0300

    feat: Optional added

--- src/main/java/com/swe/libary_project/repositories/MemberRepository.java
+++ src/main/java/com/swe/libary_project/repositories/MemberRepository.java
@@ -4,10 +4,12 @@ import com.swe.libary_project.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
-    Member findByUsername(String username);
+    Optional<Member> findByUsername(String username);
 
     boolean existsByUsername(String username);
 }
