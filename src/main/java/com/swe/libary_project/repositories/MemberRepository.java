757ad049f98522c53edc5d5a1ab7553759dc08da
BARKAN ANIL TEKE <200315078@ogr.cbu.edu.tr>
2024-12-17 21:38:12 +0300

    fix: deleted username function

--- src/main/java/com/swe/libary_project/repositories/MemberRepository.java
+++ src/main/java/com/swe/libary_project/repositories/MemberRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
-    Optional<Member> findByUsername(String username);
+
     Optional<Member> findByMail(String mail);
     boolean existsByMail(String mail);
 }
