baec3675421b353a9cc057ce96329236f0760530
BARKAN ANIL TEKE <200315078@ogr.cbu.edu.tr>
2024-12-17 21:41:19 +0300

    fix: changed name from username to mail

--- src/main/java/com/swe/libary_project/repositories/ReservationRepository.java
+++ src/main/java/com/swe/libary_project/repositories/ReservationRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    List<Reservation> findAllByOwnerUsername(String username);
+
+    List<Reservation> findAllByOwnerMail(String ownerMail) ;
 
 
 }
